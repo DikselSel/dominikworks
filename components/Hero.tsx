f9d7444ea030d5899091972f9deacc2a1c25d950
DikselSel <60197446+DikselSel@users.noreply.github.com>
2022-11-27 21:03:15 +0100

    naprawione oferty

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -4,7 +4,7 @@ const Hero = () =>{
     return(
         <div className="Hero">
             <h1>Cześć, jestem Dominik</h1>
-            <p>potrafię sporo rzeczy, a tym mozesz na tym skorzystać.</p>
+            <p>potrafię sporo rzeczy, a ty mozesz na tym skorzystać.</p>
             <a href="/kontkat">Kontakt <MdEmail/></a>
         </div>
     )
